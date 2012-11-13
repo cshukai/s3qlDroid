be5997b45a05492a5d94a7dc54526ce7986f874b
Shu-Kai Chang <cshukai@gmail.com>
2012-11-13 13:53:53 -0600

    forgetAboutJqueryFileUpload

--- src/org/mathbiol/s3qldroid/FileUpload.java
+++ src/org/mathbiol/s3qldroid/FileUpload.java
@@ -21,16 +21,15 @@ public class FileUpload extends SherlockActivity {
 	public static Camera getCameraInstance() {
 		Camera c = null;
 		try {
-			// Toast.makeText(Index.this,"here", Toast.LENGTH_SHORT).show();
-			c = Camera.open(0); // attempt to get a Camera instance
+			c = Camera.open(0); 
+			
 		} catch (Exception e) {
-			// Toast.makeText(Index.this,"test"+ e.getMessage(),
-			// Toast.LENGTH_SHORT).show();
-			Log.e("camera", e.getMessage());
+	
+			//Log.e("camera", e.getMessage());
 			throw new RuntimeException(e);
 
 		}
-		return c; // returns null if camera is unavailable
+		return c;
 	}
 
 	@Override
@@ -117,54 +116,23 @@ public class FileUpload extends SherlockActivity {
 
 				camera = FileUpload.getCameraInstance();
 				if (camera != null) {
-					Camera.Parameters cam_params = camera.getParameters();
-					Camera.CameraInfo cam_info = null;
-					camera.getCameraInfo(0, cam_info);
+
 					Toast.makeText(FileUpload.this,"here",
 							 Toast.LENGTH_SHORT).show();
 					camera.release();
 					
-					//error message: fata signal 11 (SIGSEGV)  code=1
-					
-					/*
-					 * 1.seems like the current code has conflict with the
-					 * setting of coexistence of back and front end cameras
-					 * 
-					 * whether webcam+emu or emu+emu
-					 * 
-					 * proof as follows:
-					 */
-					// Toast.makeText(Index.this, cam_info.facing,
-					// Toast.LENGTH_SHORT).show();
-					// Toast.makeText(Index.this, camera.getNumberOfCameras(),
-					// Toast.LENGTH_SHORT).show();
-					
-					// Toast.makeText(Index.this, "here",
-					// Toast.LENGTH_SHORT).show();
-
-					/*
-					 * reset the setting to single one camera , still has the
-					 * same problem with the following line
-					 * 
-					 * 
-					 * Toast.makeText(Index.this, "here",
-					 * Toast.LENGTH_SHORT).show();
-					 */
-
-					// Toast.makeText(Index.this, "here",
-					// Toast.LENGTH_SHORT).show();
+				
 				}
 
 				else {
-					// Toast.makeText(Index.this, "not",
-					// Toast.LENGTH_SHORT).show();
+					 Toast.makeText(FileUpload.this, "not",
+					 Toast.LENGTH_SHORT).show();
 				}
 				
 				
 
 			}
-			// Toast.makeText(Index.this, "Got click: " + item.getTitle(),
-			// Toast.LENGTH_SHORT).show();
+		
 
 			return true;
 		}
