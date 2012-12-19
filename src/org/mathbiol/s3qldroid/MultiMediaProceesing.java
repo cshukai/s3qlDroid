0d9312441b5bd0f6f2ab729e7ff0d8baacc68214
Shu-Kai Chang <cshukai@gmail.com>
2012-12-19 16:30:18 -0600

    cleanup

--- src/org/mathbiol/s3qldroid/MultiMediaProceesing.java
+++ src/org/mathbiol/s3qldroid/MultiMediaProceesing.java
@@ -1,14 +1,15 @@
 package org.mathbiol.s3qldroid;
 
-import com.keyes.youtube.OpenYouTubePlayerActivity;
 
-import android.net.Uri;
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+
+import com.keyes.youtube.OpenYouTubePlayerActivity;
 
 public class MultiMediaProceesing extends Activity {
 
